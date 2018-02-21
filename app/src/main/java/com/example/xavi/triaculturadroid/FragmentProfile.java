cc8ef8060e4c006c2c6e13b0462872ded4071921
miquel <miquel_sobrevals_@hotmail.com>
2018-02-21 16:51:11 +0100

    Fragments Pager

--- app/src/main/java/com/example/xavi/triaculturadroid/FragmentProfile.java
+++ app/src/main/java/com/example/xavi/triaculturadroid/FragmentProfile.java
@@ -12,17 +12,13 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TotalVotes} interface
+ * {@link FragmentProfile.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link TotalVotes#newInstance} factory method to
+ * Use the {@link FragmentProfile#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TotalVotes extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-    // TODO: Rename and change types of parameters
-    public TotalVotes() {
+public class FragmentProfile extends Fragment {
+    public FragmentProfile() {
         // Required empty public constructor
     }
 
@@ -32,17 +28,18 @@ public class TotalVotes extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TotalVotes.
+     * @return A new instance of fragment FragmentProfile.
      */
     // TODO: Rename and change types and number of parameters
     private static final String ARG_SECTION_NUMBER = "section_number";
-    public static TotalVotes newInstance(int sectionNumber) {
-        TotalVotes fragment = new TotalVotes();
+    public static FragmentProfile newInstance(int sectionNumber) {
+        FragmentProfile fragment = new FragmentProfile();
         Bundle args = new Bundle();
         args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +52,6 @@ public class TotalVotes extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_total_votes, container, false);
+        return inflater.inflate(R.layout.fragment_fragment_profile, container, false);
     }
-
 }
