512e6b616a11b41a7da613ef1d983ba69e607266
miquel <miquel_sobrevals_@hotmail.com>
2018-02-21 16:37:22 +0100

    Fragments Pager

---
+++ app/src/main/java/com/example/xavi/triaculturadroid/TabbetsActivity.java
@@ -0,0 +1,253 @@
+package com.example.xavi.triaculturadroid;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageButton;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabbetsActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private TabHost tabs;
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabbets);
+        List<FragmentPagerItem> list = new ArrayList<>();
+        FragmentPagerItem fpi;
+        for (int i=0; i<2;i++) {
+            fpi = new FragmentPagerItem();
+            fpi.setTitle("Pestanya "+i);
+            fpi.setEnabled(true);
+            if ( i == 0 )
+                fpi.setFragment( ProjectFragment.newInstance(0));
+            else if ( i == 1 )
+                fpi.setFragment( TotalVotes.newInstance(1));
+            else
+                System.out.println();
+            list.add(fpi);
+        }
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), list);
+        tabs=(TabHost)findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        int[] tabsIDs = new int[] {R.id.tab0,R.id.tab1,R.id.tab2,R.id.tab3};
+        TabHost.TabSpec spec;
+        for (int i=0; i<list.size();i++)
+        {
+            if (list.get(i).isEnabled()) {
+                spec = tabs.newTabSpec("mitab"+i);
+                spec.setContent(tabsIDs[i]);
+                spec.setIndicator(list.get(i).getTitle());
+                tabs.addTab(spec);
+            }
+        }
+
+
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                mViewPager.setCurrentItem(tabs.getCurrentTab());
+            }
+
+        });
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
+            {
+
+            }
+
+            @Override
+            public void onPageSelected(int position)
+            {
+                tabs.setCurrentTab(position);
+                mViewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_tabbets, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_tabbets, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class FragmentPagerItem {
+        private Fragment fragment;
+        private String title;
+        private boolean enabled;
+
+        public Fragment getFragment() {
+            return fragment;
+        }
+
+        public void setFragment(Fragment fragment) {
+            this.fragment = fragment;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        private final List<FragmentPagerItem> items;
+        public SectionsPagerAdapter(FragmentManager fm, List<FragmentPagerItem> items ) {
+            super(fm);
+            this.items = items;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            FragmentPagerItem fpi = getIt(position);
+            if( fpi == null )
+                return null;
+            else
+                return fpi.getFragment();
+        }
+
+        @Override
+        public int getCount() {
+            if( items == null )
+                return 0;
+            else
+                return items.size();
+        }
+
+        @Nullable
+        private FragmentPagerItem getIt(int position) {
+            if( items == null )
+                return null;
+            else {
+                if (0 <= position && position < items.size())
+                    return items.get(position);
+                else
+                    return null;
+            }
+
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            FragmentPagerItem fpi = getIt(position);
+            if( fpi == null )
+                return "";
+            else
+                return fpi.getTitle();
+        }
+    }
+}
