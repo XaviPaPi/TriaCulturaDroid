cf46ecdf3ceb190df214f0aedbf18821c2220af4
miquel <miquel_sobrevals_@hotmail.com>
2018-04-10 17:10:32 +0200

    Estrelles fetes

--- app/src/main/java/com/example/xavi/triaculturadroid/Adapters/AdapterHistorial.java
+++ app/src/main/java/com/example/xavi/triaculturadroid/Adapters/AdapterHistorial.java
@@ -1,6 +1,8 @@
 package com.example.xavi.triaculturadroid.Adapters;
 
+import android.app.Application;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.xavi.triaculturadroid.Data.Model.Historial;
 import com.example.xavi.triaculturadroid.Data.Model.Project;
+import com.example.xavi.triaculturadroid.Data.Model.User;
+import com.example.xavi.triaculturadroid.Data.Model.userTransfer;
 import com.example.xavi.triaculturadroid.R;
 
 import java.util.List;
@@ -19,8 +23,9 @@ import java.util.List;
 public class AdapterHistorial extends BaseAdapter {
 
     TextView LHP_data, LHP_nom_Projecte, LHP_vots;
-    ImageView valoracio;
+    ImageView LHP_valoracio;
     Context context;
+    userTransfer user;
     List<Historial> list_histo;
     private Historial histo;
     int mode;
@@ -30,7 +35,12 @@ public class AdapterHistorial extends BaseAdapter {
         this.list_histo = list_histo;
         mode =1;
     }
-
+    public AdapterHistorial(Context context, List<Historial> list_histo, userTransfer user){
+        this.context = context;
+        this.list_histo = list_histo;
+        this.user = user;
+        mode =1;
+    }
 
     @Override
     public int getCount() {
@@ -64,19 +74,51 @@ public class AdapterHistorial extends BaseAdapter {
             LayoutInflater inflator = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflator.inflate(R.layout.activity_item_historial_projects, parent, false);
         }
-
         int id_project;
-
+        LHP_valoracio = (ImageView)convertView.findViewById(R.id.IHP_stars);
         LHP_data = (TextView) convertView.findViewById(R.id.IHP_data);
         LHP_nom_Projecte = (TextView) convertView.findViewById(R.id.IHP_nom_projecte);
-        LHP_vots = (TextView) convertView.findViewById(R.id.IHP_valoracio);
 
         LHP_data.setText(list_histo.get(position).getData());
         LHP_nom_Projecte.setText(list_histo.get(position).getTitle());
-        LHP_vots.setText("" + list_histo.get(position).getRating());
+        double punts =  Math.floor(list_histo.get(position).getRating());
 
-
-
+        String puntuacio=String.valueOf(punts);
+        switch (puntuacio){
+            case "0":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_greys));
+                break;
+            case "0.5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_1_2star));
+                break;
+            case "1":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_1star));
+                break;
+            case "1.5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_2_2star));
+                break;
+            case "2":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_2star_));
+                break;
+            case "2.5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_3_2star_));
+                break;
+            case "3":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_3star_));
+                break;
+            case "3.5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_4_2star));
+                break;
+            case "4":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_4star));
+                break;
+            case "4.5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_5_2star));
+                break;
+            case "5":
+                LHP_valoracio.setImageDrawable( context.getResources().getDrawable(R.drawable.star_grey_5star));
+                break;
+        }
         return convertView;
     }
 }
